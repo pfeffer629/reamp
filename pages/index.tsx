fc02513d13694948c4f973716644b572835871ca
Mark Pfeffer <pfeffer629@gmail.com>
2022-12-22 11:39:13 -0500

    player controls

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ TimeAgo.addDefaultLocale(en)
 
 export default function Home() {
   const [isPlaying, setIsPlaying] = useState(false);
-  const [currentSong, setCurrentSong] = useState<ITrack>(() => ITrack);
+  const [currentSong, setCurrentSong] = useState<ITrack | {}>({});
   const { tracks, isLoading, isError } = usePaginatedTracksQuery(40);
   const timeAgo = new TimeAgo('en-US')
 
