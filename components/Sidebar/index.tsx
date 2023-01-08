92ab57674da94321100905da1cb5ba1c466b1bb0
javipark <111716310+javipark@users.noreply.github.com>
2023-01-08 15:31:51 -0500

    sidebar commits

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -82,7 +82,7 @@ export default function Sidebar() {
                 </div>
               </Link>
               <div className="text-[13px] pt-2 text-whiteDisabled h-10">
-                {currentTime} • {location}
+              {location} • {currentTime}
                 <br />
                 {currentDate}
               </div>
@@ -90,9 +90,9 @@ export default function Sidebar() {
             <div className="my-[18px] border-b border-darkLine w-full h-1"></div>
             <div className="flex items-center w-full space-x-[11px] text-whiteDisabled pb-[8px]">
               <div className="h-[13px] mb-[2px] w-[2px] rounded-md bg-whiteDisabled"></div>
-              <div>Discover</div>
+              <div className="text-sm">Discover</div>
             </div>
-            <div className="flex flex-col space-y-[3px]">
+            <div className="flex flex-col">
               <Link
                 href="/"
                 as="/"
@@ -124,9 +124,9 @@ export default function Sidebar() {
               <>
                 <div className="flex items-center w-full mt-[30px] space-x-[11px] text-whiteDisabled pb-[8px]">
                   <div className="h-[13px] mb-[2px] w-[2px] rounded-md bg-whiteDisabled"></div>
-                  <div>My Profile</div>
+                  <div className="text-sm">My Profile</div>
                 </div>
-                <div className="flex flex-col space-y-[3px]">
+                <div className="flex flex-col">
                   <Link
                     href="/favorites"
                     as="/favorites"
@@ -155,6 +155,21 @@ export default function Sidebar() {
                       </div>
                     </div>
                   </Link>
+                  <Link
+                    href="/collection"
+                    as="/collection"
+                    className="w-[177px] h-[31px] rounded-md bg-transparent hover:bg-sidebarMenuHoverBg cursor-pointer flex items-center text-[12px] relative group transition-all duration-500"
+                  >
+                    <div className="w-[177px] h-[31px] rounded-md bg-transparent hover:bg-sidebarMenuHoverBg cursor-pointer flex items-center text-[12px] relative group transition-all duration-500">
+                      <div
+                        className="rounded-full group-hover:opacity-100 opacity-0 transition-all duration-500 bg-selectedTab absolute"
+                        style={{ inset: "41.94% 89.88% 41.94% 7.14%" }}
+                      ></div>
+                      <div className="ml-[14px] transform transition-all group-hover:ml-[24px] select-none duration-300 py-[8px]">
+                        My Collection
+                      </div>
+                    </div>
+                  </Link>
                   {/*<div className="w-[177px] h-[31px] rounded-md bg-transparent hover:bg-sidebarMenuHoverBg cursor-pointer flex items-center text-[12px] relative group transition-all duration-500">
                     <div
                       className="rounded-full group-hover:opacity-100 opacity-0 transition-all duration-500 bg-selectedTab absolute"
@@ -168,7 +183,7 @@ export default function Sidebar() {
               </>
             )}
           </div>
-          <div className="pb-[104px]">
+          <div className="pb-[65px]">
             <div className="w-[177px] h-[31px] rounded-md bg-transparent hover:bg-sidebarMenuHoverBg cursor-pointer flex items-center text-[12px] relative group transition-all duration-500">
               <div
                 className="rounded-full group-hover:opacity-100 opacity-0 transition-all duration-500 bg-selectedTab absolute"
@@ -178,6 +193,17 @@ export default function Sidebar() {
                 Submit Feedback
               </div>
             </div>
+
+            <div className="w-[177px] h-[31px] rounded-md bg-transparent hover:bg-sidebarMenuHoverBg cursor-pointer flex items-center text-[12px] relative group transition-all duration-500">
+              <div
+                className="rounded-full group-hover:opacity-100 opacity-0 transition-all duration-500 bg-selectedTab absolute"
+                style={{ inset: "41.94% 89.88% 41.94% 7.14%" }}
+              ></div>
+              <div className="ml-[14px] transform transition-all group-hover:ml-[24px] select-none duration-300 py-[8px]">
+                Join the Team
+              </div>
+            </div>
+
             <div className="w-[177px] h-[31px] rounded-md space-x-4 flex items-center text-[12px] relative group transition-all duration-500 pl-[14px]">
               <a target="_blank" href="https://twitter.com/reamp__">
                 <div className="cursor-pointer hover:text-selectedTab transition-all duration-300">
@@ -186,8 +212,8 @@ export default function Sidebar() {
                     fill="currentColor"
                     strokeWidth="0"
                     viewBox="0 0 512 512"
-                    height="1em"
-                    width="1em"
+                    height="1.3em"
+                    width="1.3em"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"></path>
@@ -201,8 +227,8 @@ export default function Sidebar() {
                     fill="currentColor"
                     strokeWidth="0"
                     viewBox="0 0 24 24"
-                    height="1em"
-                    width="1em"
+                    height="1.3em"
+                    width="1.3em"
                     xmlns="http://www.w3.org/2000/svg"
                   >
                     <g>
