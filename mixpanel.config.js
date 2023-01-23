4f8884d1c6c79f298adf1b683498230805ba1cdf
javipark <111716310+javipark@users.noreply.github.com>
2023-01-23 15:38:33 -0500

    mixpanel config

---
+++ mixpanel.config.js
@@ -0,0 +1,7 @@
+import mixpanel from 'mixpanel-browser';
+
+// grab the Mixpanel factory
+var Mixpanel = require('mixpanel');
+
+// create an instance of the mixpanel client
+var mixpanel = Mixpanel.init('<67c1da43b7f22da2f0eb2558f6d5ed58>');
