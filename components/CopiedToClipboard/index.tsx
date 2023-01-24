49cb942ec63fc9ded425d5984e786fa0b0c3499f
Mark Pfeffer <pfeffer629@gmail.com>
2023-01-24 12:28:21 -0500

    fixed copied to clipboard

--- components/CopiedToClipboard/index.tsx
+++ components/CopiedToClipboard/index.tsx
@@ -15,7 +15,7 @@ function CopiedToClipboard() {
 
   return (
     <div
-      className={`opacity-${opacity} w-[100vw] top-0 absolute transition-opacity ease-in duration-200`}
+      className={`opacity-${opacity} w-[100vw] top-0 fixed transition-opacity ease-in duration-200`}
     >
       <div className="w-[200px] h-[50px] left-0 right-0 mt-[20px] mx-auto bg-sidebarBg border border-darkLine rounded-[10px] flex items-center justify-center">
         <span className="text-selectedTab flex items-center">
