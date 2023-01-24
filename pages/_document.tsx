ada5dd9bf1b9b3e3b8c29edbd663df11a0e91b3b
javipark <111716310+javipark@users.noreply.github.com>
2023-01-23 19:09:18 -0500

    metadata updates

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="DISCOVER, CONNECT, COLLECT" />
+        <meta name="description" content="Reamp.xyz | Discover, Listen, and Collect Music NFTs" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
