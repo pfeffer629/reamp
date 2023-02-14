61910c23c01437b96ba9f1b40919ca25593bcbfd
Mark Pfeffer <pfeffer629@gmail.com>
2023-02-14 15:49:27 -0500

    manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,8 +13,8 @@ export default function Document() {
         <link rel="manifest" href="/manifest.json" />
         <meta property="og:image" content="/images/Reamp_Cover_BG.png" />
         <meta property="twitter:image" content="/images/Reamp_Cover_BG.png" />
-        <meta name="theme-color" content="#34F3FF" />
-        <meta name="apple-mobile-web-app-status-bar" content="#34F3FF" />
+        <meta name="theme-color" content="#131313" />
+        <meta name="apple-mobile-web-app-status-bar" content="#131313" />
       </Head>
       <body>
         <Main />
