beb1db75fe592d3aa25b6ba4081fdc1b67993d24
Mark Pfeffer <pfeffer629@gmail.com>
2023-02-18 12:55:29 -0500

    next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
+const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache.js');
 const isProduction = process.env.NODE_ENV === 'production';
 
