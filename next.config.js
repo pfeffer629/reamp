dddb89087d1a01f731d3c5210cd6fd0b2d8324db
Mark Pfeffer <pfeffer629@gmail.com>
2023-02-27 15:27:55 -0500

    artist avatar

--- next.config.js
+++ next.config.js
@@ -6,7 +6,19 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
   reactStrictMode: false,
   images: {
-    domains: ["spinamp.mypinata.cloud", "web3-music-pipeline.mypinata.cloud", "cf-prod.noizd.com", "ipfs.io", "i.seadn.io", "arweave.net", "storageapi.fleek.co", "storageapi.fleek.one", "catalog-dev.s3.filebase.com"],
+    domains: [
+      "spinamp.mypinata.cloud", 
+      "web3-music-pipeline.mypinata.cloud", 
+      "cf-prod.noizd.com", 
+      "ipfs.io", 
+      "i.seadn.io", 
+      "arweave.net", 
+      "storageapi.fleek.co", 
+      "storageapi.fleek.one", 
+      "catalog-dev.s3.filebase.com",
+      "d2i9ybouka0ieh.cloudfront.net",
+      "catalog-prod.s3.filebase.com",
+    ],
   },
 }
 
