580895fb606e673dee710573968ae4ce7c923201
Mark Pfeffer <pfeffer629@gmail.com>
2023-03-02 10:55:33 -0500

    playbutton test

--- pages/playlists.tsx
+++ pages/playlists.tsx
@@ -84,7 +84,7 @@ export default function Playlists() {
                     onClick={(e) => handleSelectPlaylist(e, playlist.tracks)}
                   />  
                   <PlayButton
-                    className="max-sm:block hidden absolute hover:scale-125 duration-300 ease-in-out top-0 bottom-0 left-0 right-0 m-auto"
+                    // className="max-sm:block hidden absolute hover:scale-125 duration-300 ease-in-out top-0 bottom-0 left-0 right-0 m-auto"
                     height={25}
                     width={20}
                     onClick={(e) => handleSelectPlaylist(e, playlist.tracks, true)}
