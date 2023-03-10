49d7a04a22c2aad1e15b43ac74d2948536f9071a
Mark Pfeffer <pfeffer629@gmail.com>
2023-03-10 10:51:45 -0500

    collection refactor

--- components/NftCollection/index.js
+++ components/NftCollection/index.js
@@ -5,24 +5,14 @@ import { useCollectionQuery } from "@spinamp/spinamp-hooks";
 import PlayButton from "../Icons/PlayButton";
 import Image from "next/image";
 import TrackContext from "../../contexts/TrackContext";
+import PlaylistCard from "../../components/PlaylistCard";
 
 export default function NftCollection({ address }) {
   const { setCurrentTrack, setCurrentTrackIndex, setIsPlaying, setTracklist } =
     useContext(TrackContext);
   const router = useRouter();
 
-  const { data: ensAvatar } = useEnsAvatar({
-    address: address,
-  });
-  const { data: ensName } = useEnsName({ address });
   const { data, error, isLoading } = useCollectionQuery(address);
-  const svgAvatar = useMemo(
-    () =>
-      `pfp/Reamp_pfp_${
-        ["blue", "green", "orange", "yellowpink"][Math.floor(Math.random() * 4)]
-      }.svg`,
-    []
-  );
 
   if (isLoading || error) {
     return <div></div>;
@@ -43,56 +33,13 @@ export default function NftCollection({ address }) {
     <>
       {data.length > 0 &&
         data.map((track) => (
-          <div
-            key={track.id}
-            className="px-[8px] py-[10px] cursor-pointer transition-all duration-300 ease-in-out bg-transparent hover:bg-sidebarMenuHoverBg inline-block rounded-[14px] w-[223px] max-sm:w-auto max-w-full"
-          >
-            <div className="relative inline max-sm:w-auto">
-              <Image
-                src={track.lossyArtworkUrl.replace(
-                  "ipfs://",
-                  "https://ipfs.io/ipfs/"
-                )}
-                alt="nft"
-                className="w-[204px] h-[204px] max-sm:h-auto rounded-[10px]"
-                height={204}
-                width={204}
-              />
-              <div className="max-sm:hidden block">
-                <PlayButton
-                  className="absolute hover:scale-125 duration-300 ease-in-out top-0 bottom-0 left-0 right-0 m-auto"
-                  height={25}
-                  width={20}
-                  onClick={() => handleSelectTrack(track)}
-                />
-              </div>
-              <div className="max-sm:block hidden">
-                <PlayButton
-                  className="absolute hover:scale-125 duration-300 ease-in-out top-0 bottom-0 left-0 right-0 m-auto"
-                  height={25}
-                  width={20}
-                  onClick={() => handleSelectTrack(track, true)}
-                />
-              </div>
-            </div>
-            <div className="pt-2">
-              <div className="text-whiteDisabled text-[12px] uppercase">
-                Track
-              </div>
-            </div>
-            <div className="text-white truncate text-[16px]">{track.title}</div>
-            <div className="truncate w-full text-whiteDisabled text-[14px] pb-1">
-              {track.artist.name}
-            </div>
-            <div className="flex flex-row items-center space-x-[9px]">
-              <img
-                src={ensAvatar || svgAvatar}
-                alt="user"
-                className="w-[21px] aspect-square rounded-[10px]"
-              />
-              &nbsp;{ensName}
-            </div>
-          </div>
+          <PlaylistCard
+            address={address}
+            track={track}
+            onClick={() => handleSelectTrack(track)}
+            mobileOnClick={() => handleSelectTrack(track, true)}
+            collection={true}
+          />
         ))}
     </>
   );
--- components/PlaylistCard/index.tsx
+++ components/PlaylistCard/index.tsx
@@ -3,15 +3,29 @@ import PlayButton from "../../components/Icons/PlayButton";
 import TimeAgo from "javascript-time-ago";
 import Image from "next/image";
 
-function PlaylistCard({ address, playlist, onClick, mobileOnClick, type }) {
+function PlaylistCard({
+  address,
+  playlist = {},
+  track = {},
+  onClick,
+  mobileOnClick,
+  collection = false,
+}) {
   const timeAgo = new TimeAgo("en-US");
 
   return (
     <div className="px-[10px] py-[10px] cursor-pointer transition-all duration-300 ease-in-out bg-transparent hover:bg-sidebarMenuHoverBg inline-block rounded-[14px] w-[223px] max-sm:w-auto max-w-full">
       <div className="relative inline">
         <Image
-          src={playlist.cover}
-          alt="playlist"
+          src={
+            !collection
+              ? playlist.cover
+              : track.lossyArtworkUrl.replace(
+                  "ipfs://",
+                  "https://ipfs.io/ipfs/"
+                )
+          }
+          alt="cover"
           className="rounded-[10px]"
           height={204}
           width={204}
@@ -38,27 +52,39 @@ function PlaylistCard({ address, playlist, onClick, mobileOnClick, type }) {
         )}
       </div>
       <div className="pt-2">
-        <div className="text-whiteDisabled text-xs font-normal">
-          PLAYLIST • {playlist.tracks.length}{" "}
-          {playlist.tracks.length === 1 ? "Track" : "Tracks"}
+        <div className="text-whiteDisabled text-xs font-normal uppercase">
+          {!collection && <span>PLAYLIST • {playlist.tracks.length} </span>}
+          {!collection && playlist.tracks.length === 1 ? "Track" : "Tracks"}
+          {collection && "Track"}
         </div>
       </div>
-      <div className="text-white text-[20px] text-base">{playlist.name}</div>
+      <div className="text-white text-[20px] text-base">
+        {!collection ? playlist.name : track.title}
+      </div>
       <div className="flex flex-row items-center pt-1 text-sm space-x-[9px]">
         <Image
-          src={ethAccounts[playlist.user_id]["avatar"]}
+          src={
+            !collection
+              ? ethAccounts[playlist.user_id]["avatar"]
+              : ethAccounts[address]["avatar"]
+          }
           alt="user"
           className="rounded-xl"
           height={21}
           width={21}
         />
-        &nbsp;{ethAccounts[playlist.user_id]["ens"]}
+        &nbsp;
+        {!collection
+          ? ethAccounts[playlist.user_id]["ens"]
+          : ethAccounts[address]["ens"]}
       </div>
-      <div className="pt-2">
-        <div className="text-whiteDisabled text-[15px] text-xs">
-          {timeAgo.format(new Date(playlist.created_at || 0))}
+      {!collection && (
+        <div className="pt-2">
+          <div className="text-whiteDisabled text-[15px] text-xs">
+            {timeAgo.format(new Date(playlist.created_at || 0))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
