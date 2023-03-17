2aa7d4d3cc67368885f76c66596fe0ed9ecdfa49
Mark Pfeffer <pfeffer629@gmail.com>
2023-03-17 10:56:30 -0500

    checksum conversion

--- pages/checksum.js
+++ pages/checksum.js
@@ -7,23 +7,23 @@ export default function Playing() {
   const [checksum, setChecksum] = useState([]);
 
   const convert = () => {
-  	let convertedAddresses = []
-  	JSON.parse(addresses).map(tokenAddress => {
-  		let parsedAddress = tokenAddress.split('/')
-			
-  		convertedAddresses.push(`${parsedAddress[0]}/${web3.utils.toChecksumAddress(parsedAddress[1])}/${parsedAddress[2]}`)
-  	})
-  	console.log(convertedAddresses)
-  	setChecksum(convertedAddresses)
-  	console.log(checksum)
+    let convertedAddresses = []
+    JSON.parse(addresses).map(tokenAddress => {
+      let parsedAddress = tokenAddress.split('/')
+      
+      convertedAddresses.push(`${parsedAddress[0]}/${web3.utils.toChecksumAddress(parsedAddress[1])}/${parsedAddress[2]}`)
+    })
+    console.log(convertedAddresses)
+    setChecksum(convertedAddresses)
+    console.log(checksum)
   };
 
   return (
-  	<div>
-  		<textarea 
-  			className="block w-[400px] h-[400px]" 
-  			onChange={(e) => setAddresses(e.target.value)}
-			/>
+    <div>
+      <textarea 
+        className="block w-[400px] h-[400px]" 
+        onChange={(e) => setAddresses(e.target.value)}
+      />
       <button
         onClick={convert}
         className="bg-white border-darkLine border-[1px] rounded-lg w-[400px] h-[40px] text-sidebarBg"
@@ -31,6 +31,6 @@ export default function Playing() {
         Convert
       </button>
       <div className="color-white block break-normal w-[400px]">{JSON.stringify(checksum, null, 2)}</div>
-  	</div>
+    </div>
   )
 }
