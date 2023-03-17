6c70aca1b7614c26430d088d48b9e2f589073b3d
javipark <111716310+javipark@users.noreply.github.com>
2023-03-17 14:14:48 -0400

    playlist adjustments

--- pages/playlists/[id].js
+++ pages/playlists/[id].js
@@ -24,7 +24,7 @@ export default function Playlist() {
         <div className="flex flex-col justify-center">
           <div className="pt-2">
             <div className="text-whiteDisabled text-[11px]">
-              PUBLIC PLAYLIST • {selectedPlaylist?.tracks?.length} TRACKS
+              PUBLIC PLAYLIST • {selectedPlaylist?.tracks?.length} {selectedPlaylist?.tracks?.length === 1 ? 'TRACK' : 'TRACKS'}
             </div>
           </div>
           <div>
