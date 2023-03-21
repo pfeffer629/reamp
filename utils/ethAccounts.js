600b1394db8788385a9bba274e735e46bde2e634
javipark <111716310+javipark@users.noreply.github.com>
2023-03-21 14:09:53 -0400

    Update ethAccounts.js

--- utils/ethAccounts.js
+++ utils/ethAccounts.js
@@ -369,5 +369,30 @@ const ethAccounts = {
     avatar:
       "/pfp/Reamp_pfp_blue.svg",
   },
+  "0x1A1c37C145a1EaB58C43F003EBB55C18083b5987": {
+    ens: "davidtphung.eth",
+    avatar:
+      "/pfp/Reamp_pfp_blue.svg",
+  },
+  "0x29185eB8cfD22Aa719529217bFbadE61677e0Ad2": {
+    ens: "joseacabrerav.eth",
+    avatar:
+      "/pfp/Reamp_pfp_yellowpink.svg",
+  },
+  "0x48D6aaeFc242078FF5f5Fea63E71B98Dd5068818": {
+    ens: "househead.eth",
+    avatar:
+      "/pfp/Reamp_pfp_blue.svg",
+  },
+  "0x7De19fF74dBb2F208Fb3D8a19eBE7995Cef8345c": {
+    ens: "aaronsvilla.eth",
+    avatar:
+      "/pfp/Reamp_pfp_yellowpink.svg",
+  },
+  "0xd35D60B1746cAEc2289c892EAC7351D66d63455B": {
+    ens: "crittie.eth",
+    avatar:
+      "/pfp/Reamp_pfp_yellowpink.svg",
+  },
 };
 export default ethAccounts;
