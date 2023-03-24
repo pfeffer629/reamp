b485bbb05793a03ef81e42e5343cfab159dab3a1
javipark <111716310+javipark@users.noreply.github.com>
2023-03-24 19:50:01 -0400

    Update svgAvatar.js

--- utils/svgAvatar.js
+++ utils/svgAvatar.js
@@ -1,5 +1,5 @@
 const svgAvatar = `/pfp/Reamp_pfp_${
-  ["blue", "green", "orange", "yellowpink"][Math.floor(Math.random() * 4)]
+  ["blue", "orange", "yellowpink"][Math.floor(Math.random() * 4)]
 }.svg`;
 
 export default svgAvatar;
