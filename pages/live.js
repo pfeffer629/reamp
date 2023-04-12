a2eb65fca579bf5f1cf981456a094cb86288af4e
javipark <111716310+javipark@users.noreply.github.com>
2023-04-12 10:25:06 -0400

    Update live.js

--- pages/live.js
+++ pages/live.js
@@ -72,19 +72,19 @@ export default function Live() {
         </div>
         <div className="flex flex-wrap max-sm:flex-nowrap max-sm:overflow-x-scroll space-x-3 pb-[10px] tracking-wide whitespace-nowrap">
           <div className="rounded-[7px] items-center space-x-[11px] text-[14px] p-2 px-[19px] bg-sidebarBg inline-flex border border-darkLine hover:border-whiteDisabled">
-            <div className="">1,719</div>
+            <div className="">1,729</div>
             <div className="text-searchBarText">Artists</div>
           </div>
           <div className="rounded-[7px] items-center space-x-[11px] text-[14px] p-2 px-[19px] bg-sidebarBg inline-flex border border-darkLine hover:border-whiteDisabled">
-            <div className="">16,320</div>
+            <div className="">16,590</div>
             <div className="text-searchBarText">Collectors</div>
           </div>
           <div className="rounded-[7px] items-center space-x-[11px] text-[14px] p-2 px-[19px] bg-sidebarBg inline-flex border border-darkLine hover:border-whiteDisabled">
-            <div className="">93,809</div>
+            <div className="">99,342</div>
             <div className="text-searchBarText">Total Tracks</div>
           </div>
           <div className="rounded-[7px] items-center space-x-[11px] text-[14px] p-2 px-[19px] bg-sidebarBg inline-flex border border-darkLine hover:border-whiteDisabled">
-            <div className="">$13,400,340</div>
+            <div className="">$13,426,682</div>
             <div className="text-searchBarText">Total Volume (USD)</div>
           </div>
         </div>
