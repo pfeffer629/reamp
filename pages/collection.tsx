afd6ef6c42e017ee866cfeb59caaf00ca8e9258b
Andrew Colabella <acolabella11@gmail.com>
2023-05-10 15:41:03 -0600

    Onboarding (#11)
    * Basic structure WIP
    
    * favorites, playlists, collection
    
    * playlist icon sizing
    
    ---------
    
    Co-authored-by: javipark <111716310+javipark@users.noreply.github.com>

--- pages/collection.tsx
+++ pages/collection.tsx
@@ -1,11 +1,33 @@
 import { useAccount } from "wagmi";
 import NftCollection from "../components/NftCollection";
 import CardLayout from "../components/CardLayout";
+import Onboarding from "../components/Onboarding";
+import { useCollectionQuery } from "@spinamp/spinamp-hooks";
 
 export default function Collection() {
   const { address } = useAccount();
 
+  const smallHeartFilled = <img
+    src="/icons/SmallHeartFilled.svg"
+    alt="Heart Filled"
+    className="w-[32px]"
+  />
+
+  function hasCollection() {
+    if (address) {
+      const { data } = useCollectionQuery(address);
+      return data && data.length > 0;
+    }
+    else {
+      return false;
+    }
+  }
+
   return (
-    <CardLayout>{address && <NftCollection address={address} />}</CardLayout>
+    hasCollection() ?
+    <CardLayout>
+      {address && <NftCollection address={address} />}
+    </CardLayout>
+    : <Onboarding icon={smallHeartFilled} text={"You haven't collected any tracks yet"} cta={"Collect your first track"} ctaLink={"/"}/>
   );
 }
--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,9 +1,17 @@
 import { useContext } from "react";
 import Tracklist from "../components/Tracklist";
 import FavoritesContext from "../contexts/FavoritesContext";
+import Onboarding from "../components/Onboarding";
 
 export default function Favorites() {
   const { favoriteTracks } = useContext(FavoritesContext);
+  const smallHeartFilled = <img
+    src="/icons/SmallHeartFilled.svg"
+    alt="Heart Filled"
+    className="w-[32px]"
+  />
 
-  return <Tracklist tracks={favoriteTracks} />;
+  return (
+    favoriteTracks.length ? <Tracklist tracks={favoriteTracks} />
+    : <Onboarding icon={smallHeartFilled} text={"Looks like you haven't favorited any tracks yet"} cta={"Explore Tracks"} ctaLink="/"/>);
 }
