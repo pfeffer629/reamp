46694c46d7fbd0fe4a6012db795172687c434656
a-colabella <acolabella11@gmail.com>
2023-05-11 18:40:03 -0600

    track clickthrough

--- components/InfoCard/index.tsx
+++ components/InfoCard/index.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 function InfoCard({
   address,
   playlist = { cover: "", tracks: [], user_id: "", name: "", created_at: "" },
-  track = { lossyArtworkUrl: "", title: "", quantity: 0, artist: {name: "", slug: ""} },
+  track = { slug: "", lossyArtworkUrl: "", title: "", quantity: 0, artist: {name: "", slug: ""} },
   onClick,
   mobileOnClick,
   collection = false,
@@ -15,86 +15,89 @@ function InfoCard({
   const timeAgo = new TimeAgo("en-US");
 
   return (
-    <div className="px-[10px] py-[10px] cursor-pointer transition-all duration-300 ease-in-out bg-transparent hover:bg-sidebarMenuHoverBg inline-block rounded-[14px] w-[223px] max-sm:w-auto max-w-full">
-      <div className="relative inline">
-        <Image
-          src={
-            !collection
-              ? playlist.cover
-              : track.lossyArtworkUrl.replace(
-                  "ipfs://",
-                  "https://ipfs.io/ipfs/"
-                )
-          }
-          alt="cover"
-          className="rounded-[10px]"
-          height={204}
-          width={204}
-        />
-        {address && (
-          <>
-            <div className="max-sm:hidden block">
-              <PlayButton
-                className="absolute hover:scale-125 duration-300 ease-in-out top-0 bottom-0 left-0 right-0 m-auto"
-                height={25}
-                width={20}
-                onClick={onClick}
-              />
-            </div>
-            <div className="max-sm:block hidden">
-              <PlayButton
-                className="absolute hover:scale-125 duration-300 ease-in-out top-0 bottom-0 left-0 right-0 m-auto"
-                height={25}
-                width={20}
-                onClick={mobileOnClick}
-              />
-            </div>
-          </>
-        )}
-      </div>
-      <div className="pt-2">
-        <div className="text-whiteDisabled text-xs font-normal">
-          {!collection && <span>Playlist • {playlist.tracks.length} </span>}
-          {!collection && playlist.tracks.length > 1 && "Tracks"}
-          {(collection || playlist.tracks.length === 1) 
-          && <span>Track {track.quantity > 1 && `${track.quantity}- Editions`}</span>}
+    <Link href={`/tracks/${track.slug}`}>
+      <div className="px-[10px] py-[10px] cursor-pointer transition-all duration-300 ease-in-out bg-transparent hover:bg-sidebarMenuHoverBg inline-block rounded-[14px] w-[223px] max-sm:w-auto max-w-full">
+        <div className="relative inline">
+          <Image
+            src={
+              !collection
+                ? playlist.cover
+                : track.lossyArtworkUrl.replace(
+                    "ipfs://",
+                    "https://ipfs.io/ipfs/"
+                  )
+            }
+            alt="cover"
+            className="rounded-[10px]"
+            height={204}
+            width={204}
+          />
+          {address && (
+            <>
+              <div className="max-sm:hidden block">
+                <PlayButton
+                  className="absolute hover:scale-125 duration-300 ease-in-out top-0 bottom-0 left-0 right-0 m-auto"
+                  height={25}
+                  width={20}
+                  onClick={(event) => {event.preventDefault(); onClick();}}
+                />
+              </div>
+              <div className="max-sm:block hidden">
+                <PlayButton
+                  className="absolute hover:scale-125 duration-300 ease-in-out top-0 bottom-0 left-0 right-0 m-auto"
+                  height={25}
+                  width={20}
+                  onClick={(event) => {event.preventDefault(); mobileOnClick();}}
+                />
+              </div>
+            </>
+          )}
         </div>
-      </div>
-      <div className="text-white text-[20px] text-base">
-        {!collection ? playlist.name : track.title}
-      </div>
-      <div className="text-whiteDisabled text-xs font-normal hover:underline">
-        <Link
-            href={`/artists/${track.artist?.slug}`}
-          >
-          {collection && track.artist.name}
-        </Link>
-      </div>
-      <div className="flex flex-row items-center pt-1 text-sm space-x-[9px]">
-        <Image
-          src={
-            !collection
-              ? ethAccounts[playlist.user_id]["avatar"]
-              : ethAccounts[address]["avatar"]
-          }
-          alt="user"
-          className="rounded-xl"
-          height={21}
-          width={21}
-        />
-        &nbsp;
-        {!collection
-          ? ethAccounts[playlist.user_id]["ens"]
-          : ethAccounts[address]["ens"]}
-      </div>
-      {!collection && (
         <div className="pt-2">
-          <div className="text-whiteDisabled text-[15px] text-xs">
-            {timeAgo.format(new Date(playlist.created_at || 0))}
+          <div className="text-whiteDisabled text-xs font-normal">
+            {!collection && <span>Playlist • {playlist.tracks.length} </span>}
+            {!collection && playlist.tracks.length > 1 && "Tracks"}
+            {(collection || playlist.tracks.length === 1) 
+            && <span>Track {track.quantity > 1 && `- ${track.quantity} Editions`}</span>}
           </div>
         </div>
-      )}
-    </div>
+        <div className="text-white text-[20px] text-base">
+          {!collection ? playlist.name : track.title}
+        </div>
+        <div className="text-whiteDisabled text-xs font-normal hover:underline">
+          <Link
+              href={`/artists/${track.artist?.slug}`}
+            >
+            {collection && track.artist.name}
+          </Link>
+        </div>
+        <div className="flex flex-row items-center pt-1 text-sm space-x-[9px]">
+          <Image
+            src={
+              !collection
+                ? ethAccounts[playlist.user_id]["avatar"]
+                : ethAccounts[address]["avatar"]
+            }
+            alt="user"
+            className="rounded-xl"
+            height={21}
+            width={21}
+          />
+          &nbsp;
+          {!collection
+            ? ethAccounts[playlist.user_id]["ens"]
+            : ethAccounts[address]["ens"]}
+        </div>
+        {!collection && (
+          <div className="pt-2">
+            <div className="text-whiteDisabled text-[15px] text-xs">
+              {timeAgo.format(new Date(playlist.created_at || 0))}
+            </div>
+          </div>
+        )}
+      </div>
+    </Link>
+    
   );
 }
 
