c44375f514c65e41453a9c647ac7acf6860913a1
javipark <111716310+javipark@users.noreply.github.com>
2023-05-17 18:12:55 -0400

    copied to clipboard, bottom

--- components/CopiedToClipboard/index.tsx
+++ components/CopiedToClipboard/index.tsx
@@ -15,9 +15,9 @@ function CopiedToClipboard() {
 
   return (
     <div
-      className={`opacity-${opacity} w-[100vw] top-0 left-0 fixed transition-opacity ease-in duration-200`}
+      className={`opacity-${opacity} w-[100vw] bottom-20 left-0 fixed transition-opacity ease-in duration-200`}
     >
-      <div className="w-[200px] h-[50px] mt-[20px] mx-auto bg-sidebarBg border border-darkLine rounded-[10px] flex items-center justify-center">
+      <div className="w-[200px] h-[50px] mb-[30px] mx-auto bg-sidebarBg border border-darkLine rounded-[10px] flex items-center justify-center">
         <span className="text-selectedTab flex items-center">
           <div className="w-[4px] h-[4px] mr-[10px] rounded-[3px] bg-selectedTab inline-block"></div>
           Copied To Clipboard
